9246cacb706b38e33c32f048921ba64cf91f90b3
Sebastian <220244206+sebaskow@users.noreply.github.com>
2025-08-16 09:28:58 +0200

    Pierwszy commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import pyodbc
+
+conn = pyodbc.connect(
+    "Driver={ODBC Driver 18 for SQL Server};"
+    "Server=172.30.164.234,1433;" 
+    "Database=PierwszaTestowa;"
+    "UID=sa;"
+    "PWD=Karate1!xpdFFaida;"
+    "TrustServerCertificate=yes;"
+)
+
+## Sprawdzamy na docelowym połączenie
+## Test-NetConnection -ComputerName 172.130.164.234 -Port 1433
+
+## sprawdzamy na serwerze czy sam udostępnia port:
+## netstat -an | findstr :1433
+
+
+cursor = conn.cursor()
+cursor.execute("SELECT TOP 5 * FROM [dbo].[Osoby]")
+for row in cursor:
+    print(row)
